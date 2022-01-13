d2055cfe642e17eb2aabbe4cd71618d18d000951
RunaJabbar <rnjabbr@gmail.com>
2022-01-12 21:43:28 -0500

    message

---
+++ HomeCoreJava22/src/hw2Q2JavaBasics/MyCar.java
@@ -0,0 +1,13 @@
+package hw2Q2JavaBasics;
+
+public class MyCar {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Runa "+"I love to drive "+"My car is a BMW "+"It's a 2022 model "+"The color of the car is white ");
+		
+		System.out.println("\tMy name is Runa "+"I love to drive \n"+"My car is a BMW "+"It's a 2022 model "+"The color of the car is white ");
+		
+		System.out.print("My name is Runa "+"I love to drive"+"\nMy car is red");
+	}
+}
